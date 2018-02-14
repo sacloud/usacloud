4bd702468142fb6941dee84b155f060c9c47fec2
febc.yamamoto <yamamoto.febc@gmail.com>
2018-02-14 20:02:46 +0900

    Fix progress writer

--- helper/printer/printer.go
+++ helper/printer/printer.go
@@ -25,26 +25,26 @@ func Printf(c *color.Color, format string, a ...interface{}) {
 // Fprint is delegates to *color.Color or fmt depending on command.GlobalOption.NoColor flag
 func Fprint(w io.Writer, c *color.Color, a ...interface{}) {
 	if command.GlobalOption.NoColor {
-		fmt.Fprint(w, a)
+		fmt.Fprint(w, a...)
 	} else {
-		c.Fprint(w, a)
+		c.Fprint(w, a...)
 	}
 }
 
 // Fprintln is delegates to *color.Color or fmt depending on command.GlobalOption.NoColor flag
 func Fprintln(w io.Writer, c *color.Color, a ...interface{}) {
 	if command.GlobalOption.NoColor {
-		fmt.Fprintln(w, a)
+		fmt.Fprintln(w, a...)
 	} else {
-		c.Fprintln(w, a)
+		c.Fprintln(w, a...)
 	}
 }
 
 // Fprintf is delegates to *color.Color or fmt depending on command.GlobalOption.NoColor flag
 func Fprintf(w io.Writer, c *color.Color, format string, a ...interface{}) {
 	if command.GlobalOption.NoColor {
-		fmt.Fprintf(w, format, a)
+		fmt.Fprintf(w, format, a...)
 	} else {
-		c.Fprintf(w, format, a)
+		c.Fprintf(w, format, a...)
 	}
 }
