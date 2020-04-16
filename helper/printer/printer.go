ba1b3eb9a3b06c6c5caa9c48ae5495bd2d9d7f44
febc.yamamoto <yamamoto.febc@gmail.com>
2020-04-16 15:23:01 +0900

    Cleanup helper/printer

--- helper/printer/printer.go
+++ helper/printer/printer.go
@@ -22,36 +22,12 @@ import (
 	"github.com/sacloud/usacloud/command"
 )
 
-// Print is delegates to *color.Color or fmt depending on command.GlobalOption.NoColor flag
-func Print(c *color.Color, a ...interface{}) {
-	Fprint(command.GlobalOption.Out, c, a...)
-}
-
-// Println is delegates to *color.Color or fmt depending on command.GlobalOption.NoColor flag
-func Println(c *color.Color, a ...interface{}) {
-	Fprintln(command.GlobalOption.Out, c, a...)
-}
-
-// Printf is delegates to *color.Color or fmt depending on command.GlobalOption.NoColor flag
-func Printf(c *color.Color, format string, a ...interface{}) {
-	Fprintf(command.GlobalOption.Out, c, format, a...)
-}
-
 // Fprint is delegates to *color.Color or fmt depending on command.GlobalOption.NoColor flag
 func Fprint(w io.Writer, c *color.Color, a ...interface{}) {
 	if command.GlobalOption.NoColor {
 		fmt.Fprint(w, a...)
 	} else {
 		c.Fprint(w, a...)
-	}
-}
-
-// Fprintln is delegates to *color.Color or fmt depending on command.GlobalOption.NoColor flag
-func Fprintln(w io.Writer, c *color.Color, a ...interface{}) {
-	if command.GlobalOption.NoColor {
-		fmt.Fprintln(w, a...)
-	} else {
-		c.Fprintln(w, a...)
 	}
 }
 
