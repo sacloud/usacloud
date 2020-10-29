597908d46baf1cb9aec89a816a3dab611e5a654b
febc.yamamoto <yamamoto.febc@gmail.com>
2020-10-29 15:57:05 +0900

    lint: whitespace

--- pkg/self/self_linux.go
+++ pkg/self/self_linux.go
@@ -23,7 +23,6 @@ import (
 const serialFilePath = "/sys/devices/virtual/dmi/id/product_serial"
 
 func ID() (string, error) {
-
 	data, err := ioutil.ReadFile(serialFilePath)
 	if err != nil {
 		return "", err
