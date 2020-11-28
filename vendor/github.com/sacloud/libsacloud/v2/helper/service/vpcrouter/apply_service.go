d86b38aa5fff23760777a76f152b30ee2e2c8dac
febc.yamamoto <yamamoto.febc@gmail.com>
2020-11-28 11:09:30 +0900

    Commands: VPCRouter

---
+++ vendor/github.com/sacloud/libsacloud/v2/helper/service/vpcrouter/apply_service.go
@@ -0,0 +1,36 @@
+// Copyright 2016-2020 The Libsacloud Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package vpcrouter
+
+import (
+	"context"
+
+	"github.com/sacloud/libsacloud/v2/sacloud"
+)
+
+func (s *Service) Apply(req *ApplyRequest) (*sacloud.VPCRouter, error) {
+	return s.ApplyWithContext(context.Background(), req)
+}
+
+func (s *Service) ApplyWithContext(ctx context.Context, req *ApplyRequest) (*sacloud.VPCRouter, error) {
+	if err := req.Validate(); err != nil {
+		return nil, err
+	}
+
+	if req.ID.IsEmpty() {
+		return req.Builder(s.caller).Build(ctx, req.Zone)
+	}
+	return req.Builder(s.caller).Update(ctx, req.Zone, req.ID)
+}
