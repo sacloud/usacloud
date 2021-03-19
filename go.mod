248f4c60b1369d060dfa28f4f76428d5bed601ae
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2021-03-19 10:16:18 +0900

    libsacloud v2.14.1

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/sacloud/usacloud
 
-go 1.15
+go 1.16
 
 require (
 	github.com/blang/semver v3.5.1+incompatible
@@ -18,7 +18,7 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/olekukonko/tablewriter v0.0.0-20180506121414-d4647c9c7a84
 	github.com/rhysd/go-github-selfupdate v1.2.3
-	github.com/sacloud/libsacloud/v2 v2.13.0
+	github.com/sacloud/libsacloud/v2 v2.14.1
 	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
--- pkg/cmd/commands/internet/zz_create_gen.go
+++ pkg/cmd/commands/internet/zz_create_gen.go
@@ -44,7 +44,7 @@ func (p *createParameter) buildFlags(fs *pflag.FlagSet) {
 	fs.StringSliceVarP(&p.Tags, "tags", "", p.Tags, "")
 	fs.VarP(core.NewIDFlag(&p.IconID, &p.IconID), "icon-id", "", "")
 	fs.IntVarP(&p.NetworkMaskLen, "netmask", "", p.NetworkMaskLen, "(*required) options: [28/27/26] (aliases: --network-mask-len)")
-	fs.IntVarP(&p.BandWidthMbps, "band-width", "", p.BandWidthMbps, "(*required) options: [100/250/500/1000/1500/2000/2500/3000/3500/4000/4500/5000] (aliases: --band-width-mbps)")
+	fs.IntVarP(&p.BandWidthMbps, "band-width", "", p.BandWidthMbps, "(*required) options: [100/250/500/1000/1500/2000/2500/3000/3500/4000/4500/5000/5500/6000/6500/7000/7500/8000/8500/9000/9500/10000] (aliases: --band-width-mbps)")
 	fs.BoolVarP(&p.EnableIPv6, "enable-ipv6", "", p.EnableIPv6, "")
 	fs.BoolVarP(&p.NoWait, "no-wait", "", p.NoWait, "")
 	fs.IntVarP(&p.NotFoundRetry, "not-found-retry", "", p.NotFoundRetry, "")
@@ -157,7 +157,7 @@ func (p *createParameter) buildFlagsUsage(cmd *cobra.Command) {
 
 func (p *createParameter) setCompletionFunc(cmd *cobra.Command) {
 	cmd.RegisterFlagCompletionFunc("netmask", util.FlagCompletionFunc("28", "27", "26"))
-	cmd.RegisterFlagCompletionFunc("band-width", util.FlagCompletionFunc("100", "250", "500", "1000", "1500", "2000", "2500", "3000", "3500", "4000", "4500", "5000"))
+	cmd.RegisterFlagCompletionFunc("band-width", util.FlagCompletionFunc("100", "250", "500", "1000", "1500", "2000", "2500", "3000", "3500", "4000", "4500", "5000", "5500", "6000", "6500", "7000", "7500", "8000", "8500", "9000", "9500", "10000"))
 
 }
 
